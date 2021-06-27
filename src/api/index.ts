1c912efa5df94b34d60e97b1de95d5ff0be128de
James <james@limabean.co.za>
2021-06-27 18:13:24 +0200

    Write logic to get categorized movied

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,8 +3,20 @@ import axios from 'axios'
 axios.defaults.baseURL = 'https://wookie.codesubmit.io'
 axios.defaults.headers = { Authorization: 'Bearer Wookie2019' }
 
+// TODO:
 // Need to define movies interface
-export const getMovies = async () => {
+// Error handling and value checking
+// Break logic into diff files based on concerns
+export const getCategorizedMovies = async () => {
+	const response = await getMovies().then((res) => {
+		const categories = extractCategories(res.movies)
+		const categorizedMovies = categorizeMovies(res.movies, categories)
+		return categorizedMovies
+	})
+	return response
+}
+
+const getMovies = async () => {
 	const response = await axios
 		.get(`/movies`)
 		.then((res) => {
@@ -14,4 +26,23 @@ export const getMovies = async () => {
 			console.log(err)
 		})
 	return response
+}
+
+const extractCategories = (moviesArray: any[]) => {
+	return moviesArray.reduce((ac: any, cv: any) => {
+		const filteredGeneres = cv.genres.filter((genere: string) => {
+			return !ac.includes(genere)
+		})
+		ac.push(...filteredGeneres)
+		return ac
+	}, [])
+}
+
+const categorizeMovies = (moviesArray: any[], categoriesArray: any[]) => {
+	return categoriesArray.map((cat: string) => {
+		const cattedMovies = moviesArray.filter((movie: any) => {
+			return movie.genres.includes(cat)
+		})
+		return { category: cat, movies: cattedMovies }
+	})
 }
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,20 +1,16 @@
 import { FC, useState, useEffect } from 'react'
 import { HomeLayout } from './styles'
-import { getMovies } from '../../api'
+import { getCategorizedMovies } from '../../api'
 
 const Home: FC = () => {
 	const [movieList, setMovieList] = useState([])
 	useEffect(() => {
-		getMovies().then((res) => {
-			setMovieList(res.movies)
+		getCategorizedMovies().then((res: any) => {
+			setMovieList(res)
 		})
 	}, [])
-	const movies = movieList.map((val: any) => <h1>{val.title}</h1>)
-	return (
-		<HomeLayout className="main-container">
-			<div>{movies}</div>
-		</HomeLayout>
-	)
+	// const movies = movieList.map((val: any) => <h1>{val.title}</h1>)
+	return <HomeLayout className="main-container">{/* <div>{movies}</div> */}</HomeLayout>
 }
 
 export default Home
